fcc58b948468a087f43661573571304d3b6bb5ca
James Fera <james.fera@gmail.com>
2020-05-24 21:51:49 -0400

    Logging utilities

---
+++ EntityFrameworkCore.SqlChangeTracking/Logging/LoggingUtils.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Logging;
+
+namespace EntityFrameworkCore.SqlChangeTracking.Logging
+{
+    public static class LoggingUtils
+    {
+        public static bool LogErrorWithContext(this ILogger logger, Exception ex, string message, params object[] args)
+        {
+            logger.LogError(ex, message, args);
+            return true;
+        }
+        
+    }
+}
