4a7dd32c4502776bc9f4c2eeb8604ebdb40298ff
James Fera <james.fera@gmail.com>
2021-01-08 11:39:17 -0500

    add GetCurrentChangeTrackingVersion extension method

---
+++ EntityFrameworkCore.SqlChangeTracking/Extensions/DbContextExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using EntityFrameworkCore.SqlChangeTracking.Extensions.Internal;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityFrameworkCore.SqlChangeTracking.Extensions
+{
+    public static class DbContextExtensions
+    {
+        public static async ValueTask<long?> GetCurrentChangeTrackingVersion(this DbContext dbContext)
+        {
+            var reader = (await dbContext.Database.ExecuteSqlQueryAsync("SELECT CHANGE_TRACKING_CURRENT_VERSION()").ConfigureAwait(false)).DbDataReader;
+
+            while (await reader.ReadAsync().ConfigureAwait(false))
+                return reader[0] as long?;
+
+            return null;
+        }
+    }
+}
